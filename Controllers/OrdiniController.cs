8413d9dbe7a9d01909f2b4a08bd6e8d2374ff364
ramonasecci <ramonasecci@live.it>
2024-03-11 16:53:36 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/OrdiniController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Pizzeria.Models;
+
+namespace Pizzeria.Controllers
+{
+    public class OrdiniController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: Ordini
+        public ActionResult Index()
+        {
+            var ordini = db.Ordini.Include(o => o.Users);
+            return View(ordini.ToList());
+        }
+
+        // GET: Ordini/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ordini ordini = db.Ordini.Find(id);
+            if (ordini == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ordini);
+        }
+
+        // GET: Ordini/Create
+        public ActionResult Create()
+        {
+            ViewBag.User_ID = new SelectList(db.Users, "User_ID", "Nome");
+            return View();
+        }
+
+        // POST: Ordini/Create
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Ordine_ID,Indirizzo,Note,CostoCons,User_ID")] Ordini ordini)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ordini.Add(ordini);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.User_ID = new SelectList(db.Users, "User_ID", "Nome", ordini.User_ID);
+            return View(ordini);
+        }
+
+        // GET: Ordini/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ordini ordini = db.Ordini.Find(id);
+            if (ordini == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.User_ID = new SelectList(db.Users, "User_ID", "Nome", ordini.User_ID);
+            return View(ordini);
+        }
+
+        // POST: Ordini/Edit/5
+        // Per la protezione da attacchi di overposting, abilitare le proprietà a cui eseguire il binding. 
+        // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Ordine_ID,Indirizzo,Note,CostoCons,User_ID")] Ordini ordini)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ordini).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.User_ID = new SelectList(db.Users, "User_ID", "Nome", ordini.User_ID);
+            return View(ordini);
+        }
+
+        // GET: Ordini/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ordini ordini = db.Ordini.Find(id);
+            if (ordini == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ordini);
+        }
+
+        // POST: Ordini/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ordini ordini = db.Ordini.Find(id);
+            db.Ordini.Remove(ordini);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
